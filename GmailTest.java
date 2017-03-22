847fd9c929a24999114f217ef32bed4047b971a3
rcmaddali <ravichandra.maddali@gmail.com>
2017-03-22 14:15:58 -0400

    Add files via upload

---
+++ GmailTest.java
@@ -0,0 +1,41 @@
+package test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.junit.Test;
+import org.junit.Before;
+
+import org.junit.After;
+public class GmailTest {
+
+	@Before
+	public void setup(){System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
+	
+	}
+	@Test
+	public void test() {
+		
+		WebDriver driver=new ChromeDriver();
+		// Open Google Mail
+				driver.get("http://www.gmail.com");				
+				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+				driver.findElement(By.id("Email")).sendKeys("ravichandra.maddali@gmail.com");
+				driver.findElement(By.id("next")).click();
+				driver.findElement(By.id("Passwd")).sendKeys("Ravi2014");
+				driver.findElement(By.id("signIn")).click();
+				driver.findElement(By.xpath("//div[contains(text(),'COMPOSE')]")).click();
+		        driver.findElement(By.xpath("//textarea[@name='to']")).sendKeys("naveenb29@gmail.com");
+		        driver.findElement(By.xpath("//input[@name='subjectbox']")).sendKeys("test email using Selenium Script");
+		        //driver.findElement(By.xpath("//div[contains(text(),'Send')]")).click();
+		
+	}
+
+		@After
+		public void Complete(){
+			WebDriver driver=new ChromeDriver();
+			//Quit execution
+			driver.quit();
+		}
+}
